289920b0566262bc77e8d500e7efcbc409cabc60
ehds <ds_hale@163.com>
2019-12-26 09:38:42 +0800

    [+] gitignore and [-] useless files

--- push.bat
+++ push.bat
@@ -1,8 +1,15 @@
 @echo off
+echo "pull origin"
+git pull origin master
+
 echo "generating path"
 python genDirectory.py
-git pull origin master
-set /p input=please enter commit:
+
 git add .
+set /p input=please enter commit message:
+
 git commit -m "%input%"
+echo "pushing the latest commit"
+
 git push -u origin master
+echo "success"
